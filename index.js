af643e058827228f9ac29b409d3872196afd7bb1
Shawn Brown <brownology@gmail.com>
2017-11-13 20:40:29 -0800

    added configurations so cf push would work

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const bodyparser = require('body-parser');
 
 
-const users = [
-	{
+const users = [{
         "id": 1,
         "name": "Jordan",
         "age": 54
@@ -34,78 +33,75 @@ const user = new userNS.user(users);
 app.use(bodyparser.json());
 //app.use(bodyparser.urlencoded({ extended:true}));
 app.use((request, response, next) => {
-	console.log(request.headers);
-	next();
+    console.log(request.headers);
+    next();
 });
 
 app.use((request, response, next) => {
-	request.chance = Math.random();
-	next();
+    request.chance = Math.random();
+    next();
 });
 
 
-app.get('/', (request,response) => {
-	throw new Error('Oops');
-	response.json({
-		chance: request.chance
-	});
+app.get('/', (request, response) => {
+    throw new Error('Oops');
+    response.json({
+        chance: request.chance
+    });
 });
 
 app.get('/users', (request, response) => {
-	let users = user.users;
+    let users = user.users;
 
-	if(users.length > 0){
-		response.json(users);
-	}
-	else{
-		response.json({"error":"User list is empty."});
-	}
+    if (users.length > 0) {
+        response.json(users);
+    } else {
+        response.json({ "error": "User list is empty." });
+    }
 
 });
 
 app.get('/user/:id', (request, response) => {
-	let id = Number(request.params.id);
-	let item = {};
-	console.log(`user param ${id}`);
-	
-	item = user.getUser(id);
+    let id = Number(request.params.id);
+    let item = {};
+    console.log(`user param ${id}`);
 
-	if(typeof(item.id) === 'undefined'){
-		console.log('user not found');
-		throw new Error('user not found');
-	}
-	else{
-		response.json(item);
-	}
-		
+    item = user.getUser(id);
+
+    if (typeof(item.id) === 'undefined') {
+        console.log('user not found');
+        throw new Error('user not found');
+    } else {
+        response.json(item);
+    }
+
 });
 
 app.post('/user', (request, response, next) => {
-	if(typeof(request.body) !== 'undefined'){
-		let user = request.body;
-		let id = users.length > 0 ?users[users.length-1].id + 1 : 1;
-		console.log(`posted user:`);
-		console.log(user);
-		users.push({
-			id: id,
-			name: user.name,
-			age: user.age
-		});
-	}
-	else{
-		console.log('request.body was undefined');
-		console.log(request.body);
+    if (typeof(request.body) !== 'undefined') {
+        let user = request.body;
+        let id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+        console.log(`posted user:`);
+        console.log(user);
+        users.push({
+            id: id,
+            name: user.name,
+            age: user.age
+        });
+    } else {
+        console.log('request.body was undefined');
+        console.log(request.body);
 
-		throw new Error('user data not received.');
-	}
+        throw new Error('user data not received.');
+    }
 
-	response.json(users[users.length-1]);
+    response.json(users[users.length - 1]);
 });
 //the error middleware must be last
 app.use((err, request, response, next) => {
-	//log the error for now just console.log it
-	console.log(err);
-	response.status(500).send(err);
+    //log the error for now just console.log it
+    console.log(err);
+    response.status(500).send(err);
 });
-app.listen(8080);
-
+//CF requires using the PORT environment variable
+app.listen(process.env.PORT || 3000);
